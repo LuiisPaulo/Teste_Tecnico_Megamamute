33ac3f008597ff8805cf71caf2e34e116b9a4b01
LuisPaulo <luispaulolopesg@gmail.com>
2025-04-10 21:52:30 -0300

    Inicio do projeto

---
+++ BasicEcommerceProjetc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BasicEcommerceProjetc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
